7ffc50a213b95532c2f97d246aed56b3e06673d1
sadowb <90420943+sadowb@users.noreply.github.com>
2021-12-11 18:58:20 +0100

    i just started testing these files

--- output.c
+++ output.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
 int main ()
 {
     FILE *fp;
-    fp = fopen("test2.txt","W");
+    fp = fopen("test.txt","r");
+    char SingleLine[300];
+    while (!feof(fp)) // feof is file end of file this while tells the computer to run until it reaches the end of the file
+    {
+        fgets(SingleLine,300,fp);
+        puts(SingleLine);
+        strl
+    }
+    
+    fclose(fp);
     printf("this is a test");
     return 0;
 }
