5afe0504f8933ad1a930a703aca3e442ee558ba0
brainchemist2005 <93470261+brainchemist2005@users.noreply.github.com>
2021-12-11 17:45:25 +0100

    My current progress I think I can finish it in the following two days

--- zak.c
+++ zak.c
@@ -1,5 +1,67 @@
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
 int main(){
-    printf("\n");
+    int total_amount=0;
+    int amount[5]={0},result[5]={0};
+    char word[7];
+    char answer[]={"yes"},choice[7];
+    char menu[5][20]={{"Book"}
+        ,{"SkinCare"}
+        ,{"Watch"}
+        ,{"Desk"}
+        ,{"Laptop"}
+    };
+    int price[5]={200,700,1000,4000,7000};
+    printf("Welcome, if you want to make an order, type 'order'; if you want to change your information,type 'change': ");
+    scanf("%s",word);
+        if(strcmp(word,"order") == 0){
+            printf("Here is the menu:\n");
+            for(int i=0;i<5;i++){
+                printf("%s for %d MAD\t",menu[i],price[i]);
+            }
+            while(strcmp(answer,"yes")==0){
+                printf("\nType what you want to buy based on the menu: ");
+                scanf("%s",choice);
+                for(int i=0;i<7;i++){
+                tolower(choice[i]);
+                }
+                if(strcmp(choice,"book")==0){
+                    printf("How many books do you want? ");
+                    scanf("%d",&amount[0]);
+                    result[0]=amount[0]*200;
+                }
+                else if(strcmp(choice,"skincare")==0){
+                    printf("How many skin care do you want? ");
+                    scanf("%d",&amount[1]);
+                    result[1]=amount[1]*700;
+                }
+                else if(strcmp(choice,"watch")==0){
+                    printf("How many watches do you want? ");
+                    scanf("%d",&amount[2]);
+                    result[2]=amount[2]*1000;
+                }
+                else if(strcmp(choice,"desk")==0){
+                    printf("How many desks do you want? ");
+                    scanf("%d",&amount[3]);
+                    result[3]=amount[3]*4000;
+                }
+                else if(strcmp(choice,"laptop")==0){
+                    printf("How many laptops do you want? ");
+                    scanf("%d",&amount[4]);
+                    result[4]=amount[4]*6000;
+                }
+                for(int i=0;i<5;i++){
+            total_amount+=result[i];
+            }
+                printf("\nDo you want to order anything else(type yes), or do you want to proceed to the payment?(type no)");
+                scanf("%s",answer);
+            }
+            printf("\nThese items have been added to your cart.");
+            printf("\nThe total amount to pay is %d MAD",total_amount);
+            }
+            return 0;
+        }
+        
     
-}
