277ed67f571d719c796e2e7706f0a4ff0111fd3c
brainchemist2005 <93470261+brainchemist2005@users.noreply.github.com>
2021-12-14 19:11:11 +0100

    Lat update 14/12/21

--- zak.c
+++ zak.c
@@ -3,12 +3,14 @@
 #include<ctype.h>
 
 int main(){
+    FILE *fp;
+    fp=fopen("database.txt","r");
     int total_amount=0;
     int amount[5]={0},result[5]={0};
     char word[7];
-    char answer[]={"yes"},choice[7];
+    char answer[]={"yes"},choice[10];
     char menu[5][20]={{"Book"}
-        ,{"SkinCare"}
+        ,{"Skin Care"}
         ,{"Watch"}
         ,{"Desk"}
         ,{"Laptop"}
@@ -16,6 +18,7 @@ int main(){
     int price[5]={200,700,1000,4000,7000};
     printf("Welcome, if you want to make an order, type 'order'; if you want to change your information,type 'change': ");
     scanf("%s",word);
+    strlwr(word);
         if(strcmp(word,"order") == 0){
             printf("Here is the menu:\n");
             for(int i=0;i<5;i++){
@@ -23,33 +26,30 @@ int main(){
             }
             while(strcmp(answer,"yes")==0){
                 printf("\nType what you want to buy based on the menu: ");
-                scanf("%s",choice);
-                int i=0;
-                while(choice[i]!='\0'){
-                choice[i]=tolower( choice[i]);
-                i++;
-            }
-                if(strcmp(choice,"book")==0){
+                getchar();
+                gets(choice);
+                strlwr(choice);
+                if(strcmp(choice,"book")==0 || strcmp(choice,"book ")==0 ){
                     printf("How many books do you want? ");
                     scanf("%d",&amount[0]);
                     result[0]=amount[0]*200;
                 }
-                else if(strcmp(choice,"skincare")==0){
+                else if(strcmp(choice,"skin care")==0 || strcmp(choice,"skin care ")==0){
                     printf("How many skin care do you want? ");
                     scanf("%d",&amount[1]);
                     result[1]=amount[1]*700;
                 }
-                else if(strcmp(choice,"watch")==0){
+                else if(strcmp(choice,"watch")==0 || strcmp(choice,"watch ")==0){
                     printf("How many watches do you want? ");
                     scanf("%d",&amount[2]);
                     result[2]=amount[2]*1000;
                 }
-                else if(strcmp(choice,"desk")==0){
+                else if(strcmp(choice,"desk")==0 || strcmp(choice,"desk ")==0){
                     printf("How many desks do you want? ");
                     scanf("%d",&amount[3]);
                     result[3]=amount[3]*4000;
                 }
-                else if(strcmp(choice,"laptop")==0){
+                else if(strcmp(choice,"laptop")==0 || strcmp(choice,"laptop")==0){
                     printf("How many laptops do you want? ");
                     scanf("%d",&amount[4]);
                     result[4]=amount[4]*6000;
@@ -63,16 +63,14 @@ int main(){
             printf("\nThese items have been added to your cart.");
             printf("\nThe total amount to pay is %d MAD",total_amount);
             }
-            if(balance>=total_amount){
-                balance-=total_amount;
-                printf("\nThe transaction has been done ");
+            
+            if(strcmp(word,"change") == 0){
+                char c;
+                c = fgetc(fp);
+                while(c!=EOF){
+                    printf("%s",c);
+                    c = fgetc(fp);
+                }
             }
-            for(int i=0;i<2;i++){
-            else if(balance<=total_amount){
-                printf("You can't buy these items insufficent balance, try another account");
-                printf("\nEnter another account:");
-                scanf("");
-            }
-            }
-            return 0;
+return 0;
         }
