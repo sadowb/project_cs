fa79193e01a80511c140542070f399324db12c77
AmineChekayri <95981738+AmineChekayri@users.noreply.github.com>
2021-12-18 20:11:39 +0100

    Update Changeinfo.c

--- Changeinfo.c
+++ Changeinfo.c
@@ -3,70 +3,76 @@
 
 int main()
 {
-    char change_fname[50], change_lname[50], change_password1[50], change_password2[50]; 
-    char change_email[50], change_bankid[50], yes_no[5];
-    char name_changes[5], password_changes[5], email_changes[5], bankid_changes[5];
+    char change_fname[50], change_lname[50], change_password1[50], change_password2[50], org_pass_check[50]; 
+    char change_email[50], change_bankid[50], yes_no[5], info_check[40];
 
-        printf("Would you Like to Change information? 1 = Yes | 2 = No ");
-        scanf("%c", yes_no);
+        printf("Would you Like to Change information? Yes | No: ");
+        scanf("%c", info_check[40]);
 
-    if (yes_no[5] == 1)
+    if (info_check[5] == 1)
     {
-        printf("Do You Want to Change your Name? 1=Yes| 2= No");
-        scanf("%c", name_changes);
+        printf("To Change your First Name Press | 1"\n);
+        printf ("To Change your Last Name Press | 2\n");
+        printf("To Change your Password Press | 3\n");
+        printf ("To Change your Email Press | 4\n");
+        printf("To Change your Bank Account Press | 5");
+        scanf("%c", yes_no); 
     
-    if (name_changes == 1)
+    switch (yes_no)
     {
+    case '1' : 
         printf("Change First Name:");
         scanf("%c", change_fname);
+    case '2' : 
         printf("Change Last Name: ");
         scanf("%c", change_lname);
-    }else 
+    case '3' : 
+        printf("Original Password");
+        scanf("%c", org_pass_check);
+            if (strcmp(org_pass_check, password) == 0)
+            {
+                printf("Correct Original Password");
+            }else 
+                printf("Incorrect Original Password");
+                printf("Re-enter original Password");
+                scanf("%c", org_pass_check);
 
-    if (name_changes == 2)
-        printf("Do You Want to Change your Name? 1=Yes| 2= No");
-        scanf(" %c", &password_changes);
-
-    {    
         printf("Change Password: ");
         scanf("%c", change_password1);
         printf("Re-enter Password: ");
         scanf("%c", change_password2);
-   
+            if (change_password1 != change_password2)
+            {
+                printf("Password does not match:");
+                printf("Please Enter a Matching Password: ");
 
-    if (change_password1 != change_password2)
-    {
-        printf("Password does not match:");
-        printf("Please Enter a Matching Password: ");
-    }else if (change_password1 == change_password2)
-    {
-        printf("*********Correct**Password**Input*********");
-    } 
-    if (password_changes == 2)
-        printf("Do You Want to Change your Name? 1=Yes| 2= No");
-        scanf("%c", email_changes);
-    if (email_changes == 1)
-    {
+                printf("Re-enter Password: ");
+                scanf("%c", change_password2);
+            }
+            else 
+                if (change_password1 == change_password2)
+                {
+                    printf("*********Password has been changed*********");
+                } 
+    case '4' : 
         printf("Change email: ");
         scanf("%c", change_email);
-    
-    if (email_changes == 2)
-        printf("Do You Want to Change your Name? 1=Yes| 2= No");
-        scanf("%c", bankid_changes);
-    
-    }else
-    if (bankid_changes==1){
+    case '5' : 
         printf("Change Bank Account: ");
         scanf("%c", change_bankid);
-    }
-    }else 
-    if (bankid_changes ==2)
+    
+    
+    }else
+    if (info_check == 2)
+    {
         strcpy(new_lname, change_fname);
         strcpy(new_fname, change_lname);
         strcpy(new_password, change_email);
         strcpy(bank_account, change_bankid);
-}else{
+    }
     return 0;
     
+}
+
 }
 
